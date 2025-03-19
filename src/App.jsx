3612c0c8166398240a4361394d908080503c13c5
Asadbek.G <asadbekgulomov2010@gmail.com>
2025-03-19 18:24:12 +0500

    Revert "Main"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import MainRoutes from "./routes/MainRoutes.jsx";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  return <MainRoutes />;
+  return (
+    <>
+      <h1>hello world</h1>
+    </>
+  );
 }
 
 export default App;
--- src/pages/auth/Signup.jsx
+++
@@ -1,9 +0,0 @@
-const Signup = () => {
-  return (
-    <>
-      <h1>MukhammadYusuf: Signup</h1>
-    </>
-  );
-};
-
-export default Signup;
